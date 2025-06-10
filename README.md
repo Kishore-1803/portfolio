5bb769004245791d41c61599133b1f1b9ac40fca
Kishore <kishorebalajisivani@gmail.com>
2025-06-10 17:30:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 💼 Personal Portfolio Website
+
+This is the source code for my personal portfolio, built with [Next.js](https://nextjs.org) and hosted on [Vercel](https://vercel.com).
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Kishore's Portfolio",
+  description: "Portfolio of Kishore",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
